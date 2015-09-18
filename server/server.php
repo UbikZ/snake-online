44b74f246b83cdc3fed7f7c7919278a6941a546c
Gabriel Malet <gabrielmalet@gmail.com>
2015-09-18 16:31:55 +0200

    Change server.php

--- server/server.php
+++ server/server.php
@@ -1,10 +1,9 @@
-#!/usr/bin/env php
 <?php
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $app = new Ratchet\App('localhost', 8000);
 $app->route('/chat', new \Ubikz\SnakeOnline\Application\Chat());
