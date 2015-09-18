36b642c69d8cafce8ad79ac2cd75cacb385f2de3
Gabriel Malet <gabrielmalet@gmail.com>
2015-09-18 16:25:47 +0200

    Add @

--- server/server.php
+++ server/server.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 
 require __DIR__ . '/vendor/autoload.php';
 
-$app = new Ratchet\App('localhost', 8080);
+$app = new Ratchet\App('localhost', 8000);
 $app->route('/chat', new \Ubikz\SnakeOnline\Application\Chat());
 $app->route('/echo', new Ratchet\Server\EchoServer, array('*'));
 $app->run();
