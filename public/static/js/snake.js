d15fe7ffdb159cd11621b20b1f8cf1e1102bb1eb
Gabriel Malet <gabrielmalet@gmail.com>
2015-09-22 15:23:26 +0200

    Fix snake size

--- public/static/js/snake.js
+++ public/static/js/snake.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
   var h = $("#canvas").height();
 
   //Lets save the cell width in a variable for easy control
-  var cw = 5;
+  var cw = 10;
   var d;
 
   //Lets create the snake now
