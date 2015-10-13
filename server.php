66e6851cff75da5f665851db6df269efe6e4f427
Gabriel Malet <gabrielmalet@gmail.com>
2015-10-13 12:15:04 +0200

    Checkstyle

--- server.php
+++ server.php
@@ -3,10 +3,10 @@
 use Ratchet\Server\IoServer;
 use Ubikz\MMS\Chat;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 $server = IoServer::factory(
-    new \Ratchet\Http\HttpServer(new \Ratchet\WebSocket\WsServer(new Chat ())),
+    new \Ratchet\Http\HttpServer(new \Ratchet\WebSocket\WsServer(new Chat())),
     3000
 );
 
